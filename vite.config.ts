fbf34be3918a17f8a292d62202845ee60a56c3f6
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-06-25 03:06:24 +0800

    feat: update deployment workflow to run tests before building project

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:
-    process.env.NODE_ENV === "production" ? "/binance-websocket-demo/" : "/",
+  base: "/binance-websocket-demo/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
